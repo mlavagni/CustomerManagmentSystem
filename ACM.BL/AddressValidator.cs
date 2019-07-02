9ac80bbb86a6e030ba45b77e1d4b788d74ccd614
Mark Lavagni <mark_lavagni@hotmail.com>
2019-07-01 22:38:32 -0500

    Creating Model and Data projects. Separating validators from models

---
+++ ACM.BL/AddressValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class AddressValidator
+    {   
+       
+        /// <summary>
+        /// Validates the address data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate(string postalCode)
+        {
+            var isValid = true;
+
+            if (postalCode == null) isValid = false;
+            return isValid;
+        }
+    }
+}
